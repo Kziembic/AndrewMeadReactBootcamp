0cc09918f84117d202795b0b94dbe0ec577730ef
Kziembic <krzysztof.ziembicki@interia.pl>
2020-02-11 19:10:47 +0100

    conditional rendering implemented

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,8 +3,9 @@
 console.log('App.js is running!');
 
 var app = {
-    title: 'Beautiful title',
-    subtitle: 'Better subtitle'
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 //JSX - JavaScript XML
@@ -16,10 +17,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
         'ol',
@@ -39,9 +45,20 @@ var template = React.createElement(
 
 var user = {
     name: 'Krzysztof',
-    age: 26,
-    location: 'Wroclaw'
+    age: 31,
+    location: 'Philadelphia'
 };
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
 
 var templateTwo = React.createElement(
     'div',
@@ -49,20 +66,15 @@ var templateTwo = React.createElement(
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoute = document.getElementById('app');
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,15 +1,17 @@
 console.log('App.js is running!')
 
 const app = {
-    title: 'Beautiful title',
-    subtitle: 'Better subtitle'
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 //JSX - JavaScript XML
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -19,18 +21,24 @@ var template = (
 
 const user = {
     name: 'Krzysztof',
-    age: 26,
-    location: 'Wroclaw'
+    age: 31,
+    location: 'Philadelphia'
 };
 
-var templateTwo = (
+function getLocation(location) {
+    if(location) {
+        return <p>Location: {location}</p>
+    }
+}
+
+const templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
-var appRoute = document.getElementById('app');
+const appRoute = document.getElementById('app');
 
 ReactDOM.render(template, appRoute);
