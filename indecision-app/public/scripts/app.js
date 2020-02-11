afd6d80f215568e70e37582c124ec4fdfabcadea
Kziembic <krzysztof.ziembicki@interia.pl>
2020-02-11 18:46:29 +0100

    Title subtitle added

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,12 +2,69 @@
 
 console.log('App.js is running!');
 
+var app = {
+    title: 'Beautiful title',
+    subtitle: 'Better subtitle'
+};
+
 //JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var user = {
+    name: 'Krzysztof',
+    age: 26,
+    location: 'Wroclaw'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
 var appRoute = document.getElementById('app');
 
 ReactDOM.render(template, appRoute);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,36 @@
 console.log('App.js is running!')
 
+const app = {
+    title: 'Beautiful title',
+    subtitle: 'Better subtitle'
+};
+
 //JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+const user = {
+    name: 'Krzysztof',
+    age: 26,
+    location: 'Wroclaw'
+};
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
 var appRoute = document.getElementById('app');
 
 ReactDOM.render(template, appRoute);
