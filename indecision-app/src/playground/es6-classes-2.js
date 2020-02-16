c9bbc8a8dc14d18a44e155f7c4bd9b4d8f2106dc
Kziembic <krzysztof.ziembicki@interia.pl>
2020-02-16 13:33:14 +0100

    Inheritance exlpained

---
+++ indecision-app/src/playground/es6-classes-2.js
@@ -0,0 +1,65 @@
+
+class Person {
+    constructor(name = 'Anonymous', age = 0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getGreeting() {
+        return `Hi I am ${this.name}!`;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old.`
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+        if(this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+class Traveler extends Person  {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if(this.hasHomeLocation()) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`
+        }
+
+        return greeting;
+    }
+}
+
+
+const me = new Student('Krzysztof Ziembicki', 31, 'Telecommunications');
+console.log(me.getDescription());
+
+const other = new Student();
+console.log(other.getDescription());
+
+const traveler = new Traveler('Krzysztof Ziembicki', 32, 'Giedlarowa');
+console.log(traveler.getGreeting());
+
+const travelerWithoutTravel = new Traveler('Zaneta Szulhan', 31);
+console.log(travelerWithoutTravel.getGreeting());
