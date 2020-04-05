1230edfff0a57ac1fa3a574b7cc0ec39a220dc65
Kziembic <krzysztof.ziembicki@interia.pl>
2020-04-05 16:30:45 +0200

    Styles implemented.

--- indecision-app/src/components/Header.js
+++ indecision-app/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = (props) => (
   <div className="header">
-      <div>
+      <div className="container">
         <h1 className="header__title">{props.title}</h1>
         {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
       </div>
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -3,11 +3,22 @@ import { Option } from './Option';
 
 export const Options = (props) => (
       <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
+      <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+        <button
+        className="button button--link"
+         onClick={props.handleDeleteOptions}
+         >
+         Remove All
+         </button>
+         </div>
+         <div className="widget__message">
+         {props.options.length === 0 && <p>Please add an option to get started!</p>}
+         </div>
         {
-          props.options.map(option => (
+          props.options.map((option, index) => (
             <Option
+              count={index +1}
               key={option} 
               optionText={option}
               handleDeleteOption={props.handleDeleteOption}
